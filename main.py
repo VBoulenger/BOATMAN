9d5e2c547777ae23192a2dbf66d0e2f5018d5254
vincent <vincent@debian>
2022-10-05 10:00:15 +0200

    Preprocessing OK

--- main.py
+++ main.py
@@ -2,8 +2,10 @@
 First opening of Sentinel-1 SAR data
 """
 
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm.contrib import itertools
 
 from snappy import ProductIO, GPF, HashMap
 
@@ -14,17 +16,26 @@ def plot_band(product, band, vmin, vmax, clip_factor):
     w = int(band.getRasterWidth() / clip_factor)
     h = int(band.getRasterHeight() / clip_factor)
     print(w, h)
-    for i in range(clip_factor):
-        for j in range(clip_factor):
-            print(i)
-            band_data = np.zeros(w * h, np.float32)
-            band.readPixels(i * w, j * h, w, h, band_data)
+    for i, j in itertools.product(range(clip_factor), range(clip_factor)):
+        band_data = np.zeros(w * h, np.float32)
+        band.readPixels(i * w, j * h, w, h, band_data)
 
-            band_data.shape = h, w
-            plt.imsave("Test/Image{}.{}.png".format(j, i), band_data, cmap='gray', vmin=vmin, vmax=vmax, dpi=150)
+        band_data.shape = h, w
+        plt.imsave("Test/Image{}.{}.png".format(j, i), band_data, cmap='gray', vmin=vmin, vmax=vmax)
 
     return
 
+def list_params(operator_name):
+    GPF.getDefaultInstance().getOperatorSpiRegistry().loadOperatorSpis()
+    op_spi = GPF.getDefaultInstance().getOperatorSpiRegistry().getOperatorSpi(operator_name)
+    print('Op name:', op_spi.getOperatorDescriptor().getName())
+    print('Op alias:', op_spi.getOperatorDescriptor().getAlias())
+    param_Desc = op_spi.getOperatorDescriptor().getParameterDescriptors()
+    for param in param_Desc:
+        print(param.getName(), "or", param.getAlias())
+        for value in param.getValueSet():
+            print(value)
+        print('________________')
 
 
 # Set Path to Input Satellite Data
@@ -55,10 +66,52 @@ parameters.put('orbitType', 'Sentinel Precise (Auto Download)')
 parameters.put('polyDegree', '3')
 parameters.put('continueOnFail', 'False')
 
-apply_orbit_file = GPF.createProduct('Apply-Orbit-File', parameters, product)
+product = GPF.createProduct('Apply-Orbit-File', parameters, product)
 
-# Clipping
+# Calibration
 
-#TODO: Remove all lands cover from images => less memory and processing
+parameters = HashMap()
+parameters.put('outputSigmaBand', True)
+parameters.put('sourceBands', 'Intensity_VV')
+parameters.put('selectedPolarisations', "VV")
+parameters.put('outputImageScaleInDb', False)
+product = GPF.createProduct("Calibration", parameters, product)
 
-plot_band(product, "Intensity_VV", 0, 100000, 5)
+# Thermal Noise Removal
+
+parameters = HashMap()
+parameters.put('removeThermalNoise', True) 
+product = GPF.createProduct('ThermalNoiseRemoval', parameters, product)
+
+# Speckle Filtering
+
+parameters = HashMap()
+parameters.put('filter', 'Lee')
+parameters.put('filterSizeX', '5')
+parameters.put('filtersizeY', '5')
+product = GPF.createProduct('Speckle-Filter', parameters, product)
+
+# Terrain Corrections
+
+#TODO: Do not apply mask on no elevation terrain
+list_params('Terrain-Correction')
+parameters = HashMap()
+parameters.put('demName', 'SRTM 3Sec')
+parameters.put('nodataValueAtSea', False)
+
+product = GPF.createProduct('Terrain-Correction', parameters, product)
+
+# Remove GRD Border Noise
+# TODO: Nothing to do because product is alreaady calibrated ?
+
+# parameters = HashMap()
+# product = GPF.createProduct('Remove-GRD-Border-Noise', parameters, product)
+
+# Land Sea Mask
+
+parameters = HashMap()
+parameters.put('shorelineExtension', '15')
+product = GPF.createProduct('Land-Sea-Mask', parameters, product)
+
+write_format = 'BEAM-DIMAP' 
+ProductIO.writeProduct(product , 'out/test', write_format)
