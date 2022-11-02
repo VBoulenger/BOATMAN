d216bd4a743e44b6f0043a6688f6ca9405404511
VBoulenger <vincentboulenger@gmail.com>
2022-11-02 10:05:31 +0100

    Add basic map to the app

---
+++ GIS/README.md
@@ -0,0 +1,3 @@
+# PMI-GIS
+
+This project is part of the PMI project and handle the visualization of the extracted ship positions in a web-GIS. Data extraction part can be found [here](https://github.com/VBoulenger/PMI)
---
+++ GIS/app.js
@@ -0,0 +1,12 @@
+// Initialize the map
+
+var map = new L.map('map');
+
+// Set the position and zoom level of the map
+map.setView([2.5, 105], 8);
+
+// Initialize the base layer
+L.tileLayer('https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}', {
+    maxZoom: 19,
+    attribution: 'Google Satellite Hybrid'
+}).addTo(map);
