a587d89c874b94626ac68f7cb167955b0d766212
VBoulenger <vincentboulenger@gmail.com>
2022-11-29 19:30:41 +0100

    Add basic API that returns data from local database

--- server/main.py
+++ server/main.py
@@ -1,6 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.orm import Session
 import json
+
+import models
+import crud
+from database import SessionLocal, engine
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -13,8 +21,22 @@ app.add_middleware(
 )
 
 
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.get("/test.geojson")
 async def get_geosjon():
     with open("../output_data/Singapour/ship_detections_gdf.geojson") as f:
         data = json.load(f)
     return data
+
+
+@app.get("/test_db")
+def read_db(db: Session = Depends(get_db)):
+    return crud.get_detections(db)
