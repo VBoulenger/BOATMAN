65651a0301f1a277935da63cbdf9a2f5ac9a9a2a
VBoulenger <vincentboulenger@gmail.com>
2022-11-29 19:39:09 +0100

    Add connection to the database

---
+++ server/database.py
@@ -0,0 +1,20 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+PATH_DB = "detection.db"
+
+engine = create_engine(
+    "sqlite:///" + PATH_DB,
+    connect_args={"check_same_thread": False},
+    encoding='utf-8',
+    echo=False
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+if not os.path.exists(PATH_DB):
+    Base.metadata.create_all(engine)
