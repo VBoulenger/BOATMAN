5bc73271ac7d6a54b48926e94f9debae9f3b9b69
Lucas CHOLLET <lucas.chollet@free.fr>
2023-01-11 19:57:45 -0500

    Add a function to rule them all
    This function takes a `geojson` in input and:
     - Find and download the corresponding product - download_sentinel_data
     - Detect all ships on the image - process from ship_detection
     - Read the output file - result_parser
     - Amend to the database
    
     This function was the last piece needed to connect all the part and be
     able to have the intended user experience. Being selecting an area and
     seeing detected boat appearing on the screen.

--- server/main.py
+++ server/main.py
@@ -8,7 +8,11 @@ from fastapi import Depends
 from fastapi import FastAPI
 from fastapi import Request
 from fastapi.middleware.cors import CORSMiddleware
+from geojson import FeatureCollection
+from models import Tile
+from result_parser import parse_result
 from sentinel_extractor import download_sentinel_data
+from ship_detection import process
 from sqlalchemy.orm import Session
 
 app = FastAPI()
@@ -31,6 +35,31 @@ def get_db():
         db.close()
 
 
+def detect_ships_in_area(geo_dict: FeatureCollection):
+    end_req_date = date.today()
+    start_req_date = end_req_date - timedelta(days=5)
+
+    downloaded_file = download_sentinel_data(geo_dict, start_req_date, end_req_date)
+
+    if downloaded_file is None:
+        return
+
+    process(downloaded_file)
+    db_result = parse_result(downloaded_file)
+
+    # We should probably make this global
+    session = SessionLocal()
+
+    if (
+        session.query(Tile.dataset).filter_by(dataset=db_result.dataset).scalar()
+        is not None
+    ):
+        print("Product already exists in database, skipping")
+    else:
+        session.add(db_result)
+        session.commit()
+
+
 @app.get("/ships.geojson")
 def read_db(db: Session = Depends(get_db)):
     return crud.get_detections(db)
@@ -39,9 +68,7 @@ def read_db(db: Session = Depends(get_db)):
 @app.post("/polygon")
 async def get_polygon_data(req: Request):
     geo_dict = await req.json()
-    end_req_date = date.today()
-    start_req_date = end_req_date - timedelta(days=5)
-    download_sentinel_data(geo_dict, start_req_date, end_req_date)
+    detect_ships_in_area(geo_dict)
     return
 
 
