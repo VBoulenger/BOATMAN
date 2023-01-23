VBoulenger/BOATMAN

commit 12dfb30dbbec16e3eb31591880d8f4b28262a2d4

Don't use `localhost` for the python backend for Analysis and Export
This should have been done in b86fcb2d.

GIS/app.js
@@ -258,7 +258,7 @@ document.getElementById("form").addEventListener("submit", function (event) {
   searchParams.set("end_date", endDateString);
   var searchString = searchParams.toString();
 
-  var url = new URL("http://localhost:8000/ships.geojson");
+  const url = new URL(url_ships);
   url.search = searchString;
 
   $.ajax({
@@ -308,11 +308,13 @@ map.on("draw:created", function (e) {
 
 // Analysis -----------------------------------------------------------------------------
 
+const polygon_url = new URL(data_server_url + "polygon");
+
 document.getElementById("analysis").onclick = function (e) {
   // Extract GeoJson from featureGroup
   var data = drawnItems.toGeoJSON();
   $.ajax({
-    url: "http://127.0.0.1:8000/polygon",
+    url: polygon_url,
     async: true,
     type: "post",
     dataType: "json",
@@ -330,13 +332,12 @@ document.getElementById("analysis").onclick = function (e) {
 
 document.getElementById("export").onclick = function (e) {
   $.ajax({
-    url: url,
+    url: polygon_url,
     async: true,
     type: "get",
     dataType: "text",
     data: "data_type=csv",
     success: function (result) {
-      console.log(typeof result);
       result = result.slice(1, result.length - 1);
       var lines = result.split("\\n");
       var items = [];

commit 401c65ea72e8d9b469035973152e352fa60e0e54

Fix broken url to export ship detections
Regressed in 12dfb30.

GIS/app.js
@@ -319,9 +319,7 @@ document.getElementById("analysis").onclick = function (e) {
     type: "post",
     dataType: "json",
     data: JSON.stringify(data),
-    success: function (result) {
-      console.log(result);
-    },
+    success: function (result) {},
     error: function (xhr, resp, text) {
       console.log(xhr, resp, text);
     },
@@ -332,7 +330,7 @@ document.getElementById("analysis").onclick = function (e) {
 
 document.getElementById("export").onclick = function (e) {
   $.ajax({
-    url: polygon_url,
+    url: url_ships,
     async: true,
     type: "get",
     dataType: "text",