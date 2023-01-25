42dc6a6845ce6a1529c919be721ab78a6b0c3f2f
Lucas CHOLLET <lucas.chollet@free.fr>
2023-01-25 13:56:43 -0500

    Propagate error from the "/polygon" endpoint to the client
    This is done with WebSocket. The error handling on the client side is
    archaic, but it's a good start.
    
    The WebSocket connection is identified by an ID, which is generated by
    the client.

---
+++ Server/web_sockets.py
@@ -0,0 +1,43 @@
+import asyncio
+from dataclasses import dataclass
+from typing import Callable
+from typing import List
+
+from fastapi import WebSocket
+
+
+@dataclass
+class WebSocketAndId:
+    socket: WebSocket
+    id: int
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: List[WebSocketAndId] = []
+
+    async def connect(self, websocket: WebSocket, socket_id: int):
+        await websocket.accept()
+        self.active_connections.append(WebSocketAndId(websocket, socket_id))
+
+    def disconnect(self, websocket: WebSocket, socket_id: int):
+        self.active_connections.remove(WebSocketAndId(websocket, socket_id))
+
+    # notice: changed from async to sync as background tasks mess up with async functions
+    # https://stackoverflow.com/questions/73707373/how-to-return-the-result-of-backgroundtasks-as-websocket-answer-in-fastapi
+    def send_text_to_client(
+        self, client_id: int, message: str, on_no_client_found: Callable
+    ):
+        for socket_and_id in self.active_connections:
+            if socket_and_id.id == client_id:
+                #
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+
+                loop.run_until_complete(socket_and_id.socket.send_text(message))
+                return
+        on_no_client_found(client_id, message)
+
+    async def broadcast(self, message: str):
+        for connection in self.active_connections:
+            await connection.socket.send_text(message)
